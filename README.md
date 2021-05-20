c5c7846b87a5e8607009b7fbc4eb68c13893e88f
Carlos <carlosger_64@hotmail.com>
2021-05-20 17:07:16 -0500

    lab10 terminado

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Laboratorio 10 de Desarrollo de Aplicaciones Web
 
-En este laboratorio...
+En este laboratorio se trabaja con Node, Express, EJS y Mongoose, usado ahora para manipular archivos de tipo json en una página de blogs. La página cuenta con funcionalidades para agregar publicaciones, editarlas y eliminarlas.
 
 Carlos Gerardo Herrera Cortina - A00821946
--- routes/routeindex.js
+++ routes/routeindex.js
@@ -2,12 +2,12 @@ const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
 const Entry = require('../model/entry');
+var isEdit = new Boolean(false); 
 
 
 router.get('/', async function(req,res){
   var entries = await Entry.find();
-  var entrySize = entries.length;
-  res.render('index', {Entry, entrySize});
+  res.render('index', {entries});
 });
 
 
@@ -15,12 +15,50 @@ router.get('/newPost', async (req,res) =>{
   res.render('newPost');
 });
 
-router.get('/editPost', async (req,res) =>{
-  res.render('edit');
+router.post('/newPost', async (req,res) =>{
+  var entry = new Entry(req.body);
+  await entry.save();
+  /* // forma manual:
+  var entry2 = new Entry({
+    title: req.body.title,
+    author: req.body.author,
+    post_data: req.body.post_data
+  })
+  */
+  res.redirect('/');
+  res.end();
 });
 
-router.get('/deletePost', async (req,res) =>{
-  res.render('edit');
+router.get('/editPost/:id', async (req,res) =>{
+  var id = req.params.id;
+  var entry = await Entry.findById(id);
+  isEdit = true;
+  res.render('edit', {entry, isEdit});
+});
+
+router.post('/editPost/:id', async (req,res) =>{
+  await Entry.updateOne({_id: req.params.id}, {$set: {
+    title: req.body.title,
+    author: req.body.author,
+    post_data: req.body.post_data
+  }});
+  res.redirect('/');
+  res.end();
+});
+
+router.get('/deletePost/:id', async (req,res) =>{
+  var id = req.params.id;
+  var entry = await Entry.findById(id);
+  isEdit = false;
+  console.log(isEdit);
+  res.render('edit', {entry, isEdit});
+});
+
+router.post('/deletePost/:id', async (req,res) =>{
+  var id = req.params.id;
+  await Entry.deleteOne({_id: id});
+  res.redirect("/");
+  res.end();
 });
 
 
